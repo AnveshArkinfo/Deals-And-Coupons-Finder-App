3c194696e6579aa9cfc25357f067e8c275424675
AnveshArkinfo <anveshkumarinfo@gmail.com>
2021-12-14 11:19:54 +0530

    Connected to Discovery Server

---
+++ Customer/src/main/java/com/coupons/model/Customer.java
@@ -0,0 +1,56 @@
+package com.coupons.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "customer")
+public class Customer {
+
+	@Id
+	private long id;
+	private String name;
+	private String email;
+	private String password;
+	
+	public Customer(long id, String name, String email, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+	public Customer() {
+		super();
+		
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+}
